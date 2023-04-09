4c4264483d3b891f054860138b2fed90bd1f33c0
OmgD1e <Simplex.ru@mail.ru>
2023-04-09 19:31:42 +0300

    add eventListener

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,29 +6,42 @@ const Accordion = (props) => {
     const [toggle, setToggle] = useState(false)
     const [heightEl, setHeightEl] = useState();
 
-    const refHeight = useRef()
+    const refHeight = useRef();
+    const menuRef = useRef();
 
     useEffect(() => {
         setHeightEl(`${refHeight.current.scrollHeight}px`)
     }, [])
 
+    useEffect(() => {
+        let handler = (e) => {
+            if(e.target) {
+                setToggle(false);
+            }
+        };
+        document.addEventListener('mousedown', handler);
+        return() => {
+            document.removeEventListener('mousedown', handler);
+        }
+    });
+
     const toggleState = () => {
         setToggle(!toggle)
     }
     return (
-        <div>
+        <div ref={menuRef}>
             <li className='liStyle' onClick={toggleState}>
-                    <img className='marker' src= { props.img } />
-                    <p className={toggle? 'textList-active' : 'textList'}>Lorem ipsum, dolor sit amet adipisicing elit.
-                        <div
-                        className={toggle ? "accordion-toggle animated" : "accordion-toggle"}
-                        style={{height: toggle ? `${heightEl}` : '0px'}}
-                        ref={refHeight}
-                        >
-                        <p>Laboriosam quas, aut consectetur animi autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit</p>
-                        </div>
-                    </p>
-                    <img className={toggle && "active"} src={arrow} alt='accordion'/>
+                <img className='marker' src= { props.img } alt='icons' />
+                <p className={toggle? 'textList-active' : 'textList'}>Lorem ipsum, dolor sit amet adipisicing elit.
+                    <div
+                    className={toggle ? "accordion-toggle animated" : "accordion-toggle"}
+                    style={{height: toggle ? `${heightEl}` : '0px'}}
+                    ref={refHeight}
+                    >
+                    <p>Laboriosam quas, aut consectetur animi autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit</p>
+                    </div>
+                </p>
+                <img className={toggle && "active"} src={arrow} alt='accordion'/>
             </li>
         </div>
     );
